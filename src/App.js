a1b49eb358fb8f4f896d32e5df930b89f6b5e52e
EP <ep@github.com>
2021-03-06 13:53:04 -0500

    maket it better

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Better Than Yesterday
         </a>
       </header>
     </div>
