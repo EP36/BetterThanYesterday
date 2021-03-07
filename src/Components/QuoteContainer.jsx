32db04a933aa62c78b91cbdd8f7ba89f779088f0
EP <ep@github.com>
2021-03-07 12:05:12 -0500

    recolor button

--- src/Components/QuoteContainer.jsx
+++ src/Components/QuoteContainer.jsx
@@ -14,7 +14,7 @@ const api = createApi({
 
 const useStyles = makeStyles({
   button_root: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    background: 'linear-gradient(45deg, #fed2ae 5%, #FF8E53 150%)',
     border: 0,
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
